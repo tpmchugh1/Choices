cd75ab605676c3cf74fbf9ecda07c2d24750b635
Terrance McHugh <tpmchugh@mac.com>
2026-01-31 11:24:11 -0500

    Initial Commit

---
+++ Choices/ChoicesApp.swift
@@ -0,0 +1,14 @@
+//  ChoicesApp.swift
+//  Choices
+//  ©Created by Terrance McHugh on 1/31/26
+
+import SwiftUI
+
+@main
+struct ChoicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
