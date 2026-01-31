1d94e2cc2c87328261ef5dd426c8cd07ca51d847
Terrance McHugh <tpmchugh@mac.com>
2026-01-31 16:33:05 -0500

    Button Challenge for changing images and text

--- Choices/ContentView.swift
+++ Choices/ContentView.swift
@@ -5,14 +5,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var message = "Love"
+    @State private var imageName = "heart"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        Text("What's So Funny \n'Bout")
+            .multilineTextAlignment(.center)
+            .foregroundStyle(.purple)
+            .font(.largeTitle)
+            .fontWeight(.black)
+            .padding(.horizontal, 20)
+        
+        Spacer()
+        
+        VStack(spacing: 24) {
+            Image(systemName: imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 300, height: 300, alignment: .center)
+                .foregroundStyle(.purple)
+                //.border(.cyan)
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
         }
-        .padding()
+        
+        Spacer()
+        
+        HStack(spacing: 18) {
+            Button("Peace") {
+                message = "Peace"
+                imageName = "peacesign"
+            }
+            
+            Button("Love") {
+                message = "Love"
+                imageName = "heart"
+            }
+            
+            Button("Understanding") {
+                message = "Understanding"
+                imageName = "lightbulb"
+            }
+        }
+        .buttonStyle(.borderedProminent)
+        .font(.title2)
+        .tint(.purple)
+        .padding(.bottom, 18)
     }
 }
 
